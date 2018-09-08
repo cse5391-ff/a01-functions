9244298139234c95b761be241d88f3d761f872ce
Remus Tumac <rtumac@smu.edu>
2018-09-08 16:06:13 -0500

    Ex03 won finished

--- ex03.ex
+++ ex03.ex
@@ -139,7 +139,6 @@ defmodule Ex03 do
     end
   end
 
-
   ##############################################################################
   # 3.4:  5 points #
   ##################
@@ -184,10 +183,18 @@ defmodule Ex03 do
   Think a little about a nice way to lay this code out.
   """
 
-  def won do
+  def won( { a, a, a,  _, _, _,  _, _, _ } ), do: a
+  def won( { _, _, _,  a, a, a,  _, _, _ } ), do: a
+  def won( { _, _, _,  _, _, _,  a, a, a } ), do: a
 
-  end
+  def won( { a, _, _,  a, _, _,  a, _, _ } ), do: a
+  def won( { _, a, _,  _, a, _,  _, a, _ } ), do: a
+  def won( { _, _, a,  _, _, a,  _, _, a } ), do: a
 
+  def won( { a, _, _,  _, a, _,  _, _, a } ), do: a
+  def won( { _, _, a,  _, a, _,  a, _, _ } ), do: a
+  
+  def won( { _, _, _,  _, _, _,  _, _, _ } ), do: false
 
   ###########################
   # IGNORE FROM HERE TO END #
